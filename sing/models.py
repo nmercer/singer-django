a0575b047792e0bbd53959398c1bd417c56fd5d4
nmercer <nick@fancyhands.com>
2013-05-12 19:13:12 -0400

    Saving

--- sing/models.py
+++ sing/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Songs(models.Model):
     created = models.DateTimeField(auto_now_add = True)
-    url = models.CharField()
-    phone_number = models.CharField()
+    url = models.CharField(max_length=500)
+    phone_number = models.CharField(max_length=50)
     length = models.IntegerField()
     score = models.IntegerField(default=0)
